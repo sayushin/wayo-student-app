1a14582e4a4c3af634bab9538ab6d8f926488b67
sayushin <shinya.sayu@gmail.com>
2024-05-13 08:03:57 +0900

    redirect to login page

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,28 @@
-const HomePage =  () =>  {
-	return (
-		<div>
-		Home
-		</div>
-	);
-}
-export default HomePage;
+
+"use client"
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+const RedirectPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Ensure this code is only executed on the client
+    if (typeof window !== 'undefined') {
+      router.push('/login');
+    }
+  }, [router]); // Empty dependency array ensures it only runs once at component mount
+
+  return null; // Render nothing while redirecting
+};
+
+export default RedirectPage;
+
+// const HomePage =  () =>  {
+// 	return (
+// 		<div>
+// 		Home
+// 		</div>
+// 	);
+// }
+// export default HomePage;
